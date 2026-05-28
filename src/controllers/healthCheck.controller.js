6cd0dbd7b0b7c95d5e3a75c202e43c9439c2ce9c
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-05-28 19:35:01 +0530

    created async handlers to catch errors gracefully

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -1,14 +1,25 @@
 import { ApiResponse } from "../utils/api-response.js";
+import asyncHandler from "../utils/async-handler.js";
 
 
-const healthCheck = (req, res) => {
+/*
+const healthCheck = async (req, res, next) => {
     try {
+        const user = await getUserFromDB();
         res.status(200).json(
             new ApiResponse(200, {message: "API is healthy and running!"}));
         
     } catch (error) {
-        
+        next(error);
     }
 }
+*/
+
+
+const healthCheck = asyncHandler(async (req, res,) => {
+    res.status(200).json(
+        new ApiResponse(200, {message: "API is healthy and running!"})
+    )
+})
 
 export { healthCheck };
