e44fbcc0ec832850fdf7c70398be0ac3fb0d032f
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-05-30 19:58:29 +0530

    Hashed the password of userschema by using bcrypt npm dependency

--- src/models/user-models.js
+++ src/models/user-models.js
@@ -1,6 +1,6 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -59,5 +59,13 @@ const userSchema = new Schema(
     },
     { timestamps: true },
 )
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 export const User = mongoose.model("User", userSchema);
