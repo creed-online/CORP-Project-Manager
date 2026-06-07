8705959ec605b42482c79e1e017f9d8529f29e53
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-06-07 19:04:37 +0530

    Fixed correct syntax in mail.js for correct env credentials

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -14,11 +14,11 @@ const sendEmail = async (options) => {
     const eamilHtml = mailGenerator.generate(options.mailgenContent);
 
    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port: process.env.MAILTRAP_SMTP_PORT,
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            user: process.env.MAILTRAP_SMTP_USER,
+            pass: process.env.MAILTRAP_SMTP_PASS
         }
     });
 
