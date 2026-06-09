f11758cdbba399bce8a14e23131a59824fdea6fb
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-06-09 19:48:55 +0530

    Created functionality of getting the current user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -120,4 +120,17 @@ const registerUser = asyncHandler(async (req, res) => {
 
     });
 
-    export {registerUser, login, logoutUser};
+    const getCurrentUser = asyncHandler(async (req, res) => {
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                req.user,
+                "Current user fetched successfully"
+            )
+        )
+    })
+
+
+    export {registerUser, login, logoutUser, getCurrentUser};
