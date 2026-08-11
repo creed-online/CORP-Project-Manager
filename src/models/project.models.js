535e79b3b10a19e34152bef04b97e68ca357e88f
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-11 16:47:48 +0530

    Added and created project model schema

---
+++ src/models/project.models.js
@@ -0,0 +1,22 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+
+const projectSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+}, {timestamps: true});
+
+export const project = mongoose.model("Project", projectSchema);
