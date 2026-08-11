468eef8d2c6d439b90ddce32705446923cbcb266
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-11 17:07:05 +0530

    created and added subtask schema

--- src/models/subtask.models.js
+++ src/models/subtask.models.js
@@ -1,2 +1,35 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
+
+const subtaskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    task: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task",
+        required: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
+   createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    }
+}, {timestamps: true});
+
+export const subtask = mongoose.model("Subtask", subtaskSchema); 
