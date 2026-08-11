79c604ce3b056c42ccb3f55f71a81b72aeac2f06
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-11 16:53:51 +0530

    Created the project member schema and added it

--- src/models/projectmember.models.js
+++ src/models/projectmember.models.js
@@ -1,2 +1,25 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
+
+import {AvailableRoles, UseRolesEnum} from "../utils/constants.js";
+
+const projectMemberSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: AvailableRoles,
+        required: true,
+        default: UseRolesEnum.MEMBER,
+    }
+}, {timestamps: true});
+
+export const projectMember = mongoose.model("ProjectMember", projectMemberSchema); 
