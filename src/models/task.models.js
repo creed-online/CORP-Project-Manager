c955d40abea55f82de8537c8059b36e271d8fc69
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-11 17:01:51 +0530

    added and created task model schema

--- src/models/task.models.js
+++ src/models/task.models.js
@@ -1,2 +1,44 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
+import {AvailableTaskStatus, taskStatusEnum} from "../utils/constants.js";
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    status: {
+        type: String,
+        enum: AvailableTaskStatus,
+        default: taskStatusEnum.TODO
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    assignedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    },
+    attachments: {
+        type: [{
+            url: String,
+            filename: String,
+            mimetype: String,
+            size: Number
+        }],
+        default: [] 
+    }
+}, {timestamps: true});
+
+export const task = mongoose.model("Task", taskSchema); 
