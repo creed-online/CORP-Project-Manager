a28a5445ed84aefb487d5894cf65eb5cc3fd9f5c
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-11 17:10:07 +0530

    Created and added project note schema

--- src/models/note.models.js
+++ src/models/note.models.js
@@ -1,2 +1,21 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
+
+const projectNoteSchema = new Schema({
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    content: {
+        type: String,
+        trim: true
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    }
+}, {timestamps: true});
+
+export const projectNote = mongoose.model("ProjectNote", projectNoteSchema); 
