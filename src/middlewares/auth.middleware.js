d84d799d4bae38b4ba2b40ccd71b4e9266368664
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-12 20:02:25 +0530

    Created a middleware for validating Project Permissions

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,6 @@
 import {User} from "../models/user-models.js";
+import {User} from "../models/project.models.js";
+import {ProjectMember, User} from "../models/projectmember.models.js";
 import asyncHandler from "../utils/async-handler.js";
 import {ApiError} from "../utils/api-error.js";
 import jwt from "jsonwebtoken";
@@ -24,3 +26,33 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
         throw new ApiError(401, "Unauthorized. Invalid Access Token.");
     }
 });
+
+export const validateProjectPermission = asyncHandler = (roles = []) => {
+    asyncHandler(async (req, res, next) => {
+        const {projectId} = req.params;
+
+        if(!projectId) {
+            throw new ApiError(400, "Project ID is required");
+        }
+
+        await ProjectMember.findOne({
+            project: new mongoose.Types.ObjectId(projectId),
+            user: new mongoose.Types.ObjectId(req.user._id)
+        })  
+
+        id(!project) {
+            throw new ApiError(404, "Project not found");
+        }
+
+        const givenRole = project?.role;
+
+        req.user.role = givenRole;
+
+        if(!roles.includes(givenRole)) {
+            throw new ApiError(403, "Forbidden. You do not have permission to access this resource.");
+        }
+
+        next();
+
+    })
+}
