88dbb001d06b798cc0cc9d5eb046a8cf18919e6b
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-12 15:00:10 +0530

    Created mongo db aggregate pipeline for get project members

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -142,7 +142,57 @@ const addMemberToProject = asyncHandler(async(req, res) => {
 });
 
 const getProjectMembers = asyncHandler(async(req, res) => {
-//test
+    const {projectId} = req.params;
+    const project = await Project.findById(req.params);
+
+    if (!project) {
+        throw new ApiError(404, "Project not found");
+    }
+
+    const projectMembers = await ProjectMember.aggregate([
+        {
+            $match: {
+                project: new mongoose.Types.ObjectId(projectId)
+            },
+
+        },
+        
+        {
+            $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user",
+                pipeline: [
+                    {
+                        $project: {
+                            _id: 1,
+                            username: 1,
+                            email: 1,
+                            fullName: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+
+        {$addFields: {user: {$arrayElemAt: ["$user", 0]}}},
+        {
+            $project: {
+                user: 1,
+                role: 1,
+                createdAt: 1,
+                updatedAt: 1,
+                _id: 0
+            }
+        }
+        
+    ]);
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, projectMembers, "Project members fetched successfully"));
 });
 
 const updateMemberRole = asyncHandler(async(req, res) => {
