8a0ba0b9cf6ff1d71c8b50a154e6a33ec483e369
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-12 14:36:11 +0530

    Created and added functionality of adding member to project

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -114,7 +114,31 @@ const deleteProject = asyncHandler(async(req, res) => {
 });
 
 const addMemberToProject = asyncHandler(async(req, res) => {
-//test
+    const {email, role} = req.body;
+    const {projectId} = req.params;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }   
+    
+    await ProjectMember.findByIdAndUpdate({
+        project: new mongoose.Types.ObjectId(projectId),
+        user: new mongoose.Types.ObjectId(user._id),
+    },
+    {
+        project: new mongoose.Types.ObjectId(projectId),
+        user: new mongoose.Types.ObjectId(user._id),
+        role: role
+    },
+    { upsert: true, new: true }
+
+);
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Member added to project successfully"));
 });
 
 const getProjectMembers = asyncHandler(async(req, res) => {
