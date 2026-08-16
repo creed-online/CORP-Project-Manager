66ee4024223ac63488632860aab01712589922f9
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-16 10:15:18 +0530

    created and create task controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -16,6 +16,35 @@ const getTasks = asyncHandler(async (req, res) => {
 const createTask = asyncHandler(async (req, res) => {
    const { title, description, assignedTo, status } = req.body;
    const {projectId} = req.params;
+   const project = await Project.findById(projectId);
+
+   if(!project) {
+       throw new ApiError(404, "Project not found");
+   }
+
+   const files = req.files || []
+
+   const attachments = files.map((file) => {
+       return {
+        url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+        mimetype: file.mimetype,
+        size: file.size
+       }
+   })
+
+   const task = await Task.create({
+       title,
+       description,
+       project: new mongoose.Types.ObjectId(projectId),
+       assignedTo: assignedTo ? new mongoose.Types.ObjectId(assignedTo) : undefined,
+       status,
+       assignedBy: new mongoose.Types.ObjectId(req.user._id),
+       attachments
+   });
+
+   return res
+    .status(201)
+    .json(new ApiResponse(201, "Task created successfully", task))
 })
 
 const getTaskById = asyncHandler(async (req, res) => {
